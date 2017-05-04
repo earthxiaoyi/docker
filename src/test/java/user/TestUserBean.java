4704bd7018a47c2aa256e9c8816a09222bcbbcc3
earthxiaoyi@qq.com <earthxiaoyi@qq.com>
2017-05-05 00:03:35 +0800

    修改单元测试

--- src/test/java/user/TestUserBean.java
+++ src/test/java/user/TestUserBean.java
@@ -1,27 +1,30 @@
 package user;
 
+import config.dev.ApplicationConfigDev;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import user.bean.UserInfo;
 import user.service.UserService;
 
 /**
  * Created by jiaming on 2017/2/12.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationConfigDev.class})
+@ActiveProfiles("dev")
 public class TestUserBean {
+
+    @Autowired
+    private UserInfo userInfo;
 
     @Test
     public void test1(){
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("config");
-        UserInfo bean = context.getBean(UserInfo.class);
-        System.out.println(bean);
-    }
-
-    @Test
-    public void test2(){
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("config");
-        UserService userService = context.getBean(UserService.class);
-        System.out.println(userService.sayHello("小明"));
+        System.out.println(userInfo);
     }
 
 }
