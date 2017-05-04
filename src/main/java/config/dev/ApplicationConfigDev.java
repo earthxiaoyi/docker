9439ddf9966ed25a6305c84b120c5f964fceb9c7
earthxiaoyi@qq.com <earthxiaoyi@qq.com>
2017-05-05 00:11:44 +0800

    提交声明bean的代码

--- src/main/java/config/dev/ApplicationConfigDev.java
+++ src/main/java/config/dev/ApplicationConfigDev.java
@@ -2,6 +2,7 @@ package config.dev;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.*;
+import user.bean.UserInfo;
 
 /**
  * Created by jiaming on 2017/4/30.
@@ -12,4 +13,14 @@ import org.springframework.context.annotation.*;
 @Profile("dev")
 @PropertySource(value={"classpath:dev/jdbc.properties"})
 public class ApplicationConfigDev {
+
+    @Bean
+    public UserInfo userInfo(){
+        UserInfo userInfo = new UserInfo();
+        userInfo.setId(10);
+        userInfo.setPassword("pwd11");
+        userInfo.setUserName("wangerxiao");
+        return userInfo;
+    }
+
 }
